19852dfbb5934be5b71459419c1ff01a190d35d0
Roberto Brooks <rob.gate3@gmail.com>
2017-07-23 18:01:35 -0500

    modified files

--- secret.js
+++
@@ -1,6 +0,0 @@
-{
-    consumer_key:         'EZtaTnJMzGwt0BelxJJ4pvJOE';
-   consumer_secret:      'H7MY8LF3lRj4UlztDnSs8i4cJsRr48K84kd6jFaCyPci9AayEp' ;
-   access_token:         '888874425831084032-gz9waql8YusUOsMYsVynoJUxCKBMNIE' ;
-   access_token_secret:  'NqeeQMNREOdFBnoE3x0xPZ2mRZhDYWR4UNHz0Y8mbMfQI' 
-}
